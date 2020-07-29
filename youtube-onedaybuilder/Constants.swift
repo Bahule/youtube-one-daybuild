c84428880cc440cc25ad14b4c08bf335fe8c88da
Danito Mondlane <danito08mondlane@gmail.com>
2020-07-29 23:28:00 +0200

    fixed error on url expression

--- youtube-onedaybuilder/Constants.swift
+++ youtube-onedaybuilder/Constants.swift
@@ -12,7 +12,7 @@ struct Consatants {
     
     static var API_KEY = "[AIzaSyBnxxyHF6lG9MrZXek2VT9VScMUGsAPDB8]"
     static var PLAYLIST_ID = "PLMRqhzcHGw1ZkH8RuznGMS0NZs0jWQQ5a"
-    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLMRqhzcHGw1ZkH8RuznGMS0NZs0jWQQ5a=\(Consatants.PLAYLIST_ID)&key=\(Consatants.API_KEY)"
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Consatants.PLAYLIST_ID)&key=\(Consatants.API_KEY)"
     static var VIDEOCELL_ID = "VideoCell"
 }
 
